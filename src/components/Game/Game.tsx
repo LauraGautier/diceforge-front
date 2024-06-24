fbbd66c2d3c27fcaff10a3022ba3fb2996e80939
Laura Gautier <lc.gautier@icloud.com>
2024-06-24 18:01:55 +0200

    develop to master

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -42,7 +42,6 @@ function Game() {
   const currentGame = useAppSelector((state) => state.game.currentGame);
   const gameId = useAppSelector((state) => state.game.gameId);
   const gameUrl = useAppSelector((state) => state.game.gameUrl);
-  const [game, setGame] = useState<IGames>();
  
 
 
@@ -181,3 +180,6 @@ function Game() {
 }
 
 export default Game;
+
+
+
